9ed45b0044c6e7ec4c4b76fc83c829890bab45db
Exceptionall <131475748+Exceptionall@users.noreply.github.com>
2024-10-11 22:12:17 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,649 @@
+import {
+  nitk,
+  cluboard,
+  cash_flow,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  portfolio,
+  publiclab,
+  zulip,
+  cdc,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  oeclogo,
+  googlelogo,
+  ibmlogo,
+  tcslogo,
+  codtech,
+  marvel,
+  deepfake,
+  bj,
+  Layoffs1,
+  Layoffs,
+  Coffee,
+  HRDashboard,
+  Sales,
+  rollercoster,
+  HouseSales,
+  foragelogo,
+  
+
+} from "../assets";
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+  AiOutlineTwitter,
+  AiFillHtml5,
+  AiOutlineGitlab,
+  AiTwotoneDatabase,
+  AiOutlineDatabase,
+  AiFillDatabase,
+  AiOutlineRadarChart,
+  AiFillCalculator,
+  AiOutlineAreaChart,
+  AiTwotonePieChart,
+  AiFillPieChart,
+  AiOutlineDotChart,
+  AiOutlineBarChart,
+  AiFillCode,
+  AiOutlineSearch,
+  AiOutlineTable,
+  AiFillRobot,
+} from "react-icons/ai";
+
+import {
+  SiDjango,
+  SiJavascript,
+  SiBootstrap,
+  SiReact,
+  SiTailwindcss,
+  SiGraphql,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiRubyonrails,
+  SiJquery,
+  SiVisualstudiocode,
+  SiPostman,
+  SiGit,
+  SiMysql,
+  SiSolidity,
+  SiNetlify,
+  SiChartdotjs,
+  SiVite,
+  SiArduino,
+  SiWeb3Dotjs,
+  SiIpfs,
+  SiDotnet,
+  SiTwilio,
+  SiFlutter,
+  SiReplit,
+  SiFlask,
+  SiFigma,
+  SiGooglemaps,
+  SiOpenai,
+  SiGooglecloud,
+  SiNextdotjs,
+  SiMicrosoftazure,
+  SiR,
+  SiMicrosoftexcel,
+  SiGooglesheets,
+  SiGooglebigquery,
+  SiDgraph,
+  SiDatabricks,
+  SiJupyter,
+  SiKaggle
+} from "react-icons/si";
+
+import {
+  FaHardHat,
+  FaRust
+} from "react-icons/fa";
+
+import {
+  IoIosNotificationsOutline
+} from "react-icons/io";
+
+import {
+  FaGolang
+} from "react-icons/fa6";
+
+import { DiCoffeescript, DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+import { SiSqlite } from "react-icons/si";
+import { SiTableau } from "react-icons/si";
+import { SiPowerbi } from "react-icons/si";
+import { SiGoogleanalytics } from "react-icons/si";
+import { SiSimpleanalytics } from "react-icons/si";
+import { SiVisualbasic } from "react-icons/si";
+import { SiSoundcharts } from "react-icons/si";
+import { SiStackedit } from "react-icons/si";
+import { SiEditorconfig } from "react-icons/si";
+import { SiCkeditor4 } from "react-icons/si";
+import { SiDatacamp } from "react-icons/si";
+import { SiOpensearch } from "react-icons/si";
+import { SiRobotframework } from "react-icons/si";
+
+export const resumeLink = "https://drive.google.com/file/d/10C7-PO7uE9Gb6L9FWmhL4b0S7ckgbGNW/view?usp=drive_link";
+export const repoLink = "https://github.com/R-Rija";
+
+export const callToAction = "https://www.linkedin.com/in/rija-r/";
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+  {
+    id: "achievements",
+    title: "Achievements",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  
+  
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+export const educationList = [
+  {
+    id: "education-1",
+    icon: oeclogo,
+    title: "Oxford Engineering College, Trichy",
+    degree: "Bachelor of Computer Science and Engineering",
+    duration: "July 2020 - July 2024",
+    content1: "Major: Computer Science",
+    
+  },
+  {
+    id: "education-2",
+    icon: pba,
+    title: "St Sebastain Higher Secondary School",
+    degree: "12th Standard – Science Stream",
+    duration: "June 2019 - March 2020",
+    content1: "Major: Computer Science",
+  },
+];
+
+export const achievements = [
+  {
+    id: "a-1",
+    icon: googlelogo,
+    event: "Google Data Analytics Professional Certificate",
+    position: "Google",
+    content1: "An industry-recognized certification that teaches data cleaning, analysis, and visualization using tools like Excel, SQL, and Tableau.",
+    content2: " Focused on practical skills and real-world projects.",
+    
+    
+    project: "https://drive.google.com/file/d/1RHSUl_UII1WANUdG63YjolZ3QYe1Gwy7/view?usp=drive_link",
+    
+  },
+  {
+    id: "a-2",
+    icon: tcslogo,
+    event: "TCS iON Career Edge - Young Professional",
+    position: "TCS iON",
+    content1: "A career-focused course that builds foundational skills like communication, problem-solving, and business etiquette to prepare individuals for professional environments.",
+    content2: "Skills: Presentation Skills · Communication · Soft Skills",
+    content3: "",
+    project: "https://drive.google.com/file/d/1OGWjPyc2O7ee2Jn5ebvQ1S34YE12QjSR/view?usp=drive_link",
+  },
+  {
+    id: "a-3",
+    icon: ibmlogo,
+    event: "Python for Data Science",
+    position: "IBM",
+    content1: "A course focused on Python for data science, covering data manipulation, analysis, and visualization using libraries like Pandas, NumPy, and Matplotlib.",
+    content2: "Skills: Python (Programming Language) · pandas · NumPy",
+    content3: "",
+    project: "https://drive.google.com/file/d/1RMYBnKt3v0X5XgSPNRIqnOOUMLvaXAD2/view?usp=drive_link",
+  },
+  {
+    id: "a-4",
+    icon: googlelogo,
+    event: "Share Data Through the Art of Visualization",
+    position: "Google",
+    content1: "A course that teaches effective data visualization techniques to communicate insights clearly.",
+    content2: "Skills: Data Visualization · Data Storytelling · Tableau",
+    content3: "",
+    
+    project: "https://drive.google.com/file/d/1RNvVB-Y3dvvLEbs_8NCvgCZlqUtE0e8e/view?usp=drive_link",
+  },
+  {
+    id: "a-5",
+    icon: googlelogo,
+    event: "Analyze Data to Answer Questions",
+    position: "Google",
+    content1: "A course focused on data analysis techniques to extract meaningful insights.",
+    content2: "Skills: Data Analysis · Data Aggregation · Data calculations",
+    content3: "",
+    project: "https://drive.google.com/file/d/19wiDx1G85KyFIdvVJCL2hKnC7UyrJ1g9/view?usp=drive_link",
+  },
+  {
+    id: "a-5",
+    icon: foragelogo,
+    event: "Accenture North America Data Analytics and Visualization",
+    position: "Google",
+    content1: "Completed a simulation focused on advising a hypothetical social media client as a Data Analyst at Accenture",
+    content2: "Cleaned, modelled and analyzed 7 datasets to uncover insights into content trends to inform strategic decisions",
+    content3: "Prepared a PowerPoint deck and video presentation to communicate key insights for the client and internal stakeholders",
+    project: "https://drive.google.com/file/d/19wiDx1G85KyFIdvVJCL2hKnC7UyrJ1g9/view?usp=drive_link",
+  },
+  {
+    id: "a-5",
+    icon: foragelogo,
+    event: "Accenture North America Data Analytics and Visualization",
+    position: "Google",
+    content1: "Completed a simulation focused on advising a hypothetical social media client as a Data Analyst at Accenture",
+    content2: "Cleaned, modelled and analyzed 7 datasets to uncover insights into content trends to inform strategic decisions",
+    content3: "Prepared a PowerPoint deck and video presentation to communicate key insights for the client and internal stakeholders",
+    project: "https://drive.google.com/file/d/19wiDx1G85KyFIdvVJCL2hKnC7UyrJ1g9/view?usp=drive_link",
+  },
+];
+
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+      
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-3",
+        icon: SiR,
+        name: "R",
+      },
+      {
+        id: "pl-4",
+        icon: SiSqlite,
+        name: "SQL",
+      },
+      
+      {
+        id: "pl-8",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "pl-9",
+        icon: DiCss3,
+        name: "CSS",
+      },
+      
+    ],
+  },
+  {
+    title: "Data Analysis Skills",
+    items: [
+      {
+        id: "f-1",
+        icon: AiFillDatabase,
+        name: "Data Cleaning",
+      },
+      {
+        id: "f-1",
+        icon: SiDatabricks,
+        name: "Data Wrangling",
+      },
+      {
+        id: "f-2",
+        icon: SiCkeditor4,
+        name: "Data Manipulation",
+      },
+      {
+        id: "f-3",
+        icon: SiSimpleanalytics,
+        name: "Data Analysis",
+      },
+      {
+        id: "f-4",
+        icon: SiSoundcharts,
+        name: "Data Visualization",
+      },
+      
+    ],
+  },
+  {
+    title: "Tools",
+    items: [
+      {
+        id: "t-1",
+        icon: SiMicrosoftexcel,
+        name: "Excel",
+      },
+      {
+        id: "t-2",
+        icon: SiGooglesheets,
+        name: "Google Sheets",
+      },
+      {
+        id: "t-3",
+        icon: SiMysql,
+        name: "MySQL",
+      },
+      {
+        id: "t-4",
+        icon: SiGooglebigquery,
+        name: "Google Bigquery",
+      },
+      {
+        id: "t-5",
+        icon: SiTableau,
+        name: "Tableau",
+      },
+      {
+        id: "t-6",
+        icon: SiPowerbi,
+        name: "Power Bi",
+      },
+      {
+        id: "t-7",
+        icon: SiJupyter,
+        name: "Jupyter",
+      },
+      {
+        id: "t-8",
+        icon: SiVisualstudiocode,
+        name: "VS Code",
+      },
+      
+      {
+        id: "t-9",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      
+      
+      
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    organisation: "Codtech IT Solutions, India",
+    logo: codtech,
+    link: "https://www.linkedin.com/company/codtechitsolutions/",
+    positions: [
+      {
+        title: "Data Analyst Intern",
+        duration: "Aug 2024 - Oct 2024",
+        content: [
+          {
+            text: "Conducted a comprehensive analysis of a dataset to uncover trends, patterns, and insights. Utilized Python and libraries such as Pandas and Matplotlib for data manipulation and visualization, resulting in actionable recommendations that enhanced decision-making processes.",
+            link: "",
+          },
+      
+          {
+            text: "Developed complex SQL queries to extract and analyze data from relational databases. Focused on cleaning and transforming data to support business intelligence efforts, significantly improving reporting accuracy and efficiency.",
+            link: "",
+          },
+          
+        ],
+      },
+    ],
+  },
+  
+];
+
+
+
+export const projects = [
+  {
+    id: "project-1",
+    title: "CrispCrumbs Biscuit Business Analysis",
+    github: "https://github.com/R-Rija/CrispCrumbs_Biscuit_Analysis.git",
+    
+    image: Coffee,
+    content: [
+      "Creating comprehensive business analysis dashboard for CrispCrumbs and will provide insights into revenue distribution, customer demographics,geographic performance,profitability and sales performance.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiMicrosoftexcel,
+        name: "Excel"
+      },
+      {
+        id: "icon-2",
+        icon: AiFillDatabase,
+        name: "Data Cleaning"
+      },
+      {
+        id: "icon-3",
+        icon: SiOpenai,
+        name: "Data Pre-Processing"
+      },
+      {
+        id: "icon-4",
+        icon: AiFillCalculator,
+        name: "Pivot"
+      },
+      {
+        id: "icon-5",
+        icon: AiOutlineAreaChart,
+        name: "Data visualization"
+      },
+    ],
+  },
+  {
+    id: "project-2",
+    title: "HR Dashboard",
+    github: "https://github.com/R-Rija/HR_Dashboard.git",
+    
+    image: HRDashboard,
+    content: [
+
+      " The HR Dashboard project provides insights into human resources data. It aims to visualize key metrics related to employee management, performance, and attrition rates using Tableau's interactive data visualizations.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiTableau,
+        name: "Tableau"
+      },
+      {
+        id: "icon-2",
+        icon: SiIpfs,
+        name: "Data Cleaning"
+      },
+      {
+        id: "icon-3",
+        icon: SiSoundcharts,
+        name: "Data Visualization"
+      },
+      
+      
+      
+      
+    ],
+  },
+  {
+    id: "project-3",
+    title: "Retail Sales Analysis",
+    github: "https://github.com/R-Rija/Retail_Sales_Analysis.git",
+    
+    image: Sales,
+    content: [
+      "This project designed to analyze and clean retail sales data and include operations for data exploration,identifying missing values,and generating insights such as the highest monthly sales for each year.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: AiTwotoneDatabase,
+        name: "SQL"
+      },
+      {
+        id: "icon-3",
+        icon: SiMysql,
+        name: "MySQL"
+      },
+      {
+        id: "icon-4",
+        icon: AiOutlineSearch,
+        name: "Data Exploration"
+      },
+      {
+        id: "icon-2",
+        icon: SiIpfs,
+        name: "Data Cleaning"
+      },
+      
+    ],
+  },
+  {
+    id: "project-4",
+    title: "Exploratory Data Analysis For Roller Coaster",
+    github: "https://github.com/R-Rija/Exploratory_Data_Analysis_For_Roller_Coaster.git",
+    
+    image: rollercoster,
+    content: [
+      "Exploratory Data Analysis (EDA) on roller coaster datasets to identify patterns, trends, and insights into factors such as height, speed, duration, and their potential relationship to customer experiences.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiJupyter,
+        name: "Jupyter Notebook"
+      },
+      {
+        id: "icon-2",
+        icon: AiOutlineSearch,
+        name: "Data Exploration"
+      },
+      {
+        id: "icon-2",
+        icon: SiIpfs,
+        name: "Data Cleaning"
+      },
+      {
+        id: "icon-2",
+        icon: SiSoundcharts,
+        name: "Data visualization"
+      },
+    ],
+  },
+  {
+    id: "project-5",
+    title: "House Sales Dashboard",
+    github: "https://github.com/R-Rija/House_sales_dashboard.git",
+    
+    image: HouseSales,
+    content: [
+      "This dashboard is designed to help users visualize and explore trends in house prices, sales volumes, and geographical distribution.",
+    ],
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiTableau,
+        name: "Tableau"
+      },
+      {
+        id: "icon-2",
+        icon: SiIpfs,
+        name: "Data Cleaning"
+      },
+      {
+        id: "icon-2",
+        icon: SiSoundcharts,
+        name: "Data visualization"
+      },
+    ],
+  },
+  
+];
+
+
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/rija-r/",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+    link: "https://github.com/R-Rija",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "rija75217@gmail.com",
+  },
+  {
+    id: "social-media-4",
+    icon: SiTableau,
+    link: "https://public.tableau.com/app/profile/rija.r4185/vizzes",
+  },
+  {
+    id: "social-media-5",
+    icon: SiKaggle,
+    link: "https://www.kaggle.com/rijarija",
+  },
+];
+
+export const aboutMe = {
+    name: "Rija",
+    tagLine: "Aspiring Data Analyst | Data Analysis Trainee | Google Analytics Certification | Data-Driven Decision Maker| Proficient in SQL, Python, Power BI, & Tableau",
+    intro: " I am a data analyst and B.E. graduate in Computer Science and Engineering with a certification in Google Data Analytics. Skilled in SQL, Python, and data visualization, I focus on transforming complex data into actionable insights. My goal is to leverage my analytical expertise to inform decisions and make meaningful contributions to every project.",
+
+
+
+
+
+
+
+}
